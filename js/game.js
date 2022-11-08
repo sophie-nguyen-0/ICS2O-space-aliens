e37b7bf8cd584c662c06b04b1dc7d9406852deb4
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-08 19:38:13 +0000

    stuff

---
+++ js/game.js
@@ -0,0 +1,15 @@
+/* global Phaser */
+//created by sophie
+//created on nov 2022
+//this is the Phaser3 game configuration file
+
+/**
+* Start Phaser Game
+*/
+const config = {
+  type: Phaser.Auto
+  width: 1920,
+  height: 1080,
+  //set background color
+  backgroundColor: 0x5f6ea,
+}
