ba3d84309163d5e8d18279d8946742b18ecf5c1b
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-25 19:28:42 +0000

    js

--- js/game.js
+++ js/game.js
@@ -7,11 +7,14 @@
 import SplashScene from "./splashScene.js";
 import TitleScene from "./titleScene.js";
 import MenuScene from "./menuScene.js";
+import GameScene from "./gameScene.js";
+
 
 //our game scene
 const splashScene = new SplashScene();
 const titleScene = new TitleScene();
 const menuScene = new MenuScene();
+const gameScene = new GameScene();
 
 /**
  * Start Phaser Game
@@ -42,6 +45,7 @@ const game = new Phaser.Game(config);
 game.scene.add("splashScene", splashScene);
 game.scene.add("titleScene", titleScene);
 game.scene.add("menuScene", menuScene);
+game.scene.add("gameScene", gameScene);
 
 //start title
 game.scene.start("splashScene");
