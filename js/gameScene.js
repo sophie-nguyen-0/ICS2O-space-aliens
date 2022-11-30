0db8d12db92ada2dbf95548b612a8aeb1724f1de
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-30 18:44:40 +0000

    gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,6 +13,9 @@ class GameScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "gameScene" });
+
+    this.background = null
+    this.ship = null
   }
 
   /**
@@ -32,6 +35,10 @@ class GameScene extends Phaser.Scene {
    */
   preload() {
     console.log("Game Scene");
+
+    //images
+    this.load.image('starBackground', 'assets/starBackground.png')
+    this.load.image('ship', 'assets/spaceShip.png')
   }
 
   /**
@@ -39,7 +46,12 @@ class GameScene extends Phaser.Scene {
    *Use it to create your game objects
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
-  create(data) {}
+  create(data) {
+    this.background = this.add.image(0,0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0,0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+  }
 
   /**
    *Should be overridden by your own scenes
