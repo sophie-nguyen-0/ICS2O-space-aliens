acc9e2988600927e3de2338319e4449e44d61d06
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-12-03 00:46:29 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -14,8 +14,8 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" });
 
-    this.background = null
-    this.ship = null
+    this.background = null;
+    this.ship = null;
   }
 
   /**
@@ -37,8 +37,8 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene");
 
     //images
-    this.load.image('starBackground', 'assets/starBackground.png')
-    this.load.image('ship', 'assets/spaceShip.png')
+    this.load.image("starBackground", "assets/starBackground.png");
+    this.load.image("ship", "assets/spaceShip.png");
   }
 
   /**
@@ -47,10 +47,10 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
   create(data) {
-    this.background = this.add.image(0,0, 'starBackground').setScale(2.0)
-    this.background.setOrigin(0,0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
   /**
@@ -60,19 +60,19 @@ class GameScene extends Phaser.Scene {
    * @param {number} delta - The delta time in ms since the last frame
    */
   update(time, delta) {
-    const keyLeftObj = this.input.keyboard.addKey("LEFT")
-    const keyRightObj = this.input.keyboard.addKey("RIGHT")
-    
-    if(keyLeftObj.isDown == true){
-      this.ship.x -= 15
+    const keyLeftObj = this.input.keyboard.addKey("LEFT");
+    const keyRightObj = this.input.keyboard.addKey("RIGHT");
+
+    if (keyLeftObj.isDown == true) {
+      this.ship.x -= 15;
       if (this.ship.x < 0) {
-        this.ship.x = 0
+        this.ship.x = 0;
       }
     }
-    if(keyRightObj.isDown == true){
-      this.ship.x += 15
-      if(this.ship.x > 1920){
-        this.ship.x = 1920
+    if (keyRightObj.isDown == true) {
+      this.ship.x += 15;
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920;
       }
     }
   }
