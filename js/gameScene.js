a256074d800d41f430ad2175cfbc403e872e1cf8
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-12-07 18:51:21 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -42,7 +42,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("ship", "assets/spaceShip.png");
     this.load.image("missile", "assets/missile.png");
     //sound
-    this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio("laser", "assets/laser1.wav");
   }
 
   /**
@@ -94,22 +94,20 @@ class GameScene extends Phaser.Scene {
           "missile"
         );
         this.missileGroup.add(aNewMissile);
-        this.sound.play('laser')
+        this.sound.play("laser");
       }
     }
-    
+
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false;
     }
 
     this.missileGroup.children.each(function (item) {
-      item.y = item.y - 15
+      item.y = item.y - 15;
       if (item.y < 0) {
-        item.destroy()
+        item.destroy();
       }
-    }
-      
-    )
+    });
   }
 }
 
