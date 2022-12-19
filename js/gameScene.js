5e185452e2fe1edfe9f3ec5d856c252f5616dc4f
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-19 19:23:43 +0000

    js

--- js/gameScene.js
+++ js/gameScene.js
@@ -8,17 +8,18 @@
  *This class is the Game scene
  */
 class GameScene extends Phaser.Scene {
+
   //create an Alien
   createAlien() {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1;
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1;
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1;
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien");
-    anAlien.body.velocity.y = 200;
-    anAlien.body.velocity.x = alienXVelocity;
-    this.alienGroup.add(anAlien);
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : - 1
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
   }
-
+  
   /**
    *this method is the constructor
    */
@@ -28,13 +29,12 @@ class GameScene extends Phaser.Scene {
     this.background = null;
     this.ship = null;
     this.fireMissile = false;
-    this.score = 0;
-    this.scoreText = null;
-    this.scoreTextStyle = {
-      font: "65px Arial",
-      fill: "#ffffff",
-      align: "center",
-    };
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = {font: '65px Arial', fill: '#ffffff', align: "center"}
+
+    this.gameOverText = null
+    this.gameOverTextStyle = {font: '65px Arial', fill: '#ff0000', align: "center"}
   }
 
   /**
@@ -59,10 +59,11 @@ class GameScene extends Phaser.Scene {
     this.load.image("starBackground", "assets/starBackground.png");
     this.load.image("ship", "assets/spaceShip.png");
     this.load.image("missile", "assets/missile.png");
-    this.load.image("alien", "assets/alien.png");
+    this.load.image('alien', 'assets/alien.png')
     //sound
     this.load.audio("laser", "assets/laser1.wav");
-    this.load.audio("explosion", "assets/barrelExploding.wav");
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
   /**
@@ -74,12 +75,7 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
     this.background.setOrigin(0, 0);
 
-    this.scoreText = this.add.text(
-      10,
-      10,
-      "Score: " + this.score.toString(),
-      this.scoreTextStyle
-    );
+    this.scoreText = this.add.text(10,10,'Score: '+ this.score.toString(), this.scoreTextStyle)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
@@ -88,21 +84,30 @@ class GameScene extends Phaser.Scene {
 
     //create a group for the aliens
     this.alienGroup = this.add.group();
-    this.createAlien();
+    this.createAlien()
 
     //collision between missiles and aliens
-    this.physics.add.collider(
-      this.missileGroup,
-      this.alienGroup,
-      function (missileCollide, alienCollide) {
-        alienCollide.destroy();
-        missileCollide.destroy();
-        this.sound.play("explosion");
-        this.score = this.score + 1;
-        this.scoreText.setText("Score: " + this.score.toString());
-        this.createAlien(this.createAlien());
-      }.bind(this)
-    );
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function(missileCollide, alienCollide){
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.score =this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createAlien(
+      this.createAlien()
+      )
+    }.bind(this))
+
+    //collision between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function(shipCollide, alienCollide){
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCurser: true})
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+    }.bind(this))
   }
 
   /**
